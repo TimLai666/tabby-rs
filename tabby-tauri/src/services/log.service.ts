f70a332586bfa270515918197c5598d99924eba1
TimLai666 <tim930102@icloud.com>
2026-08-01 02:55:08 +0800

    lint: document public logger constructor bridge

--- tabby-tauri/src/services/log.service.ts
+++ tabby-tauri/src/services/log.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core'
 import { ConsoleLogger, Logger, LogService } from 'tabby-core'
 
 class TauriConsoleLogger extends ConsoleLogger {
+    // ConsoleLogger exposes a protected constructor; this subclass intentionally
+    // makes it public for the host LogService factory.
+    // eslint-disable-next-line @typescript-eslint/no-useless-constructor
     constructor (name: string) {
         super(name)
     }
