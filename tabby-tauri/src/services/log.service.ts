2eb5b80249f9cabf40b29c6aa7442c0a117af48f
TimLai666 <tim930102@icloud.com>
2026-08-01 02:23:19 +0800

    fix: expose constructor for Tauri console logger

--- tabby-tauri/src/services/log.service.ts
+++ tabby-tauri/src/services/log.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core'
 import { ConsoleLogger, Logger, LogService } from 'tabby-core'
 
+class TauriConsoleLogger extends ConsoleLogger {
+    constructor (name: string) {
+        super(name)
+    }
+}
+
 @Injectable()
 export class TauriLogService extends LogService {
     create (name: string): Logger {
-        return new ConsoleLogger(name)
+        return new TauriConsoleLogger(name)
     }
 }
