c0231135e667e26e911feab57da72c982fa4daf3
TimLai666 <tim930102@icloud.com>
2026-08-01 02:24:30 +0800

    feat: register Tauri Angular host providers

---
+++ tabby-tauri/src/index.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core'
+import {
+    ConfigProvider,
+    HostAppService,
+    HostWindowService,
+    LogService,
+    PlatformService,
+} from 'tabby-core'
+
+import { TauriConfigProvider } from './config'
+import { HostBridge } from './api/hostBridge'
+import { TauriHostBridge } from './services/tauriHostBridge.service'
+import { TauriHostAppService } from './services/hostApp.service'
+import { TauriHostWindowService } from './services/hostWindow.service'
+import { TauriLogService } from './services/log.service'
+import { TauriPlatformService } from './services/platform.service'
+
+@NgModule({
+    providers: [
+        TauriHostBridge,
+        { provide: HostBridge, useExisting: TauriHostBridge },
+        { provide: PlatformService, useClass: TauriPlatformService },
+        { provide: HostAppService, useClass: TauriHostAppService },
+        { provide: HostWindowService, useClass: TauriHostWindowService },
+        { provide: LogService, useClass: TauriLogService },
+        { provide: ConfigProvider, useClass: TauriConfigProvider, multi: true },
+    ],
+})
+export default class TauriModule { }
+
+export * from './api/hostBridge'
+export { TauriHostBridge }
