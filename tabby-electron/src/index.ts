b550ffa9f4828fc6db32d16f18573d24c377dc94
TimLai666 <tim930102@icloud.com>
2026-08-02 00:21:56 +0800

    fix: use correct MSYS2 shell provider name

--- tabby-electron/src/index.ts
+++ tabby-electron/src/index.ts
@@ -33,7 +33,7 @@ import { Cygwin64ShellProvider } from './shells/cygwin64'
 import { GitBashShellProvider } from './shells/gitBash'
 import { LinuxDefaultShellProvider } from './shells/linuxDefault'
 import { MacOSDefaultShellProvider } from './shells/macDefault'
-import { MSYS2ShellsProvider } from './shells/msys2'
+import { MSYS2ShellProvider } from './shells/msys2'
 import { POSIXShellsProvider } from './shells/posix'
 import { PowerShellCoreShellProvider } from './shells/powershellCore'
 import { WindowsDefaultShellProvider } from './shells/winDefault'
@@ -69,7 +69,7 @@ import { VSDevToolsProvider } from './shells/vs'
         { provide: ShellProvider, useClass: Cygwin64ShellProvider, multi: true },
         { provide: ShellProvider, useClass: GitBashShellProvider, multi: true },
         { provide: ShellProvider, useClass: POSIXShellsProvider, multi: true },
-        { provide: ShellProvider, useClass: MSYS2ShellsProvider, multi: true },
+        { provide: ShellProvider, useClass: MSYS2ShellProvider, multi: true },
         { provide: ShellProvider, useClass: WSLShellProvider, multi: true },
         { provide: ShellProvider, useClass: VSDevToolsProvider, multi: true },
 
