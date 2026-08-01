e1ef7d337718c4f90335877d568bd315ebca077d
TimLai666 <tim930102@icloud.com>
2026-08-02 01:50:44 +0800

    feat: register Tauri shell providers

--- tabby-tauri/src/index.ts
+++ tabby-tauri/src/index.ts
@@ -11,10 +11,12 @@ import {
 } from 'tabby-core'
 import { SettingsTabProvider } from 'tabby-settings'
 
+import { ShellProvider } from '../../tabby-local/src/api'
 import { PasswordStorageService } from '../../tabby-ssh/src/services/passwordStorage.service'
 import { HostBridge } from './api/hostBridge'
 import './api/keychain'
 import { SecretImporter } from './api/secretImporter'
+import './api/shell'
 import {
     IdentitySettingsTabComponent,
     IdentitySettingsTabProvider,
@@ -26,6 +28,10 @@ import { TauriLogService } from './services/log.service'
 import { TauriPasswordStorageService } from './services/passwordStorage.service'
 import { TauriPlatformService } from './services/platform.service'
 import { TauriSecretImporter } from './services/secretImporter.service'
+import {
+    TauriDetectedShellProvider,
+    TauriSpawnRequestService,
+} from './services/shellProvider.service'
 import { TauriHostBridge } from './services/tauriHostBridge.service'
 import { TauriUpdaterService } from './services/updater.service'
 import { TauriVaultService } from './services/vault.service'
@@ -46,6 +52,8 @@ import { TauriVaultService } from './services/vault.service'
         TauriSecretImporter,
         { provide: SecretImporter, useExisting: TauriSecretImporter },
         { provide: PasswordStorageService, useClass: TauriPasswordStorageService },
+        { provide: ShellProvider, useClass: TauriDetectedShellProvider, multi: true },
+        TauriSpawnRequestService,
         { provide: ConfigProvider, useClass: TauriConfigProvider, multi: true },
         { provide: SettingsTabProvider, useClass: IdentitySettingsTabProvider, multi: true },
     ],
@@ -57,7 +65,10 @@ export default class TauriModule { }
 export * from './api/hostBridge'
 export * from './api/keychain'
 export * from './api/secretImporter'
+export * from './api/shell'
+export { TauriDetectedShellProvider }
 export { TauriHostBridge }
 export { TauriPasswordStorageService }
 export { TauriSecretImporter }
+export { TauriSpawnRequestService }
 export { TauriVaultService }
