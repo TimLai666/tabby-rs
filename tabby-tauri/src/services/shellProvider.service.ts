bc656417f99246b56fba9411dce86ec7be25f8b6
TimLai666 <tim930102@icloud.com>
2026-08-02 01:50:04 +0800

    feat: adapt Rust shell detection to Tabby profiles

---
+++ tabby-tauri/src/services/shellProvider.service.ts
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { Injectable } from '@angular/core'
+import { ConfigService } from 'tabby-core'
+
+import {
+    SessionOptions,
+    Shell,
+    ShellProvider,
+} from '../../../tabby-local/src/api'
+import { HostBridge } from '../api/hostBridge'
+import {
+    DetectedShell,
+    PreparedSpawnRequest,
+} from '../api/shell'
+
+@Injectable()
+export class TauriDetectedShellProvider extends ShellProvider {
+    constructor (
+        private bridge: HostBridge,
+        private config: ConfigService,
+    ) {
+        super()
+    }
+
+    async provide (): Promise<Shell[]> {
+        const result = await this.bridge.invoke('shell.detect', {
+            identification: this.config.store?.terminal?.identification ?? null,
+        })
+        for (const warning of result.warnings) {
+            console.warn('[shell detection]', warning)
+        }
+        return result.shells.map(shell => this.toShell(shell))
+    }
+
+    private toShell (shell: DetectedShell): Shell {
+        return {
+            id: shell.id,
+            name: shell.name,
+            command: shell.command,
+            args: shell.args,
+            env: shell.env,
+            fsBase: shell.fsBase,
+            cwd: shell.cwd,
+            icon: iconFor(shell.icon),
+            shellType: shell.shellType,
+            hidden: shell.hidden,
+        }
+    }
+}
+
+@Injectable({ providedIn: 'root' })
+export class TauriSpawnRequestService {
+    constructor (private bridge: HostBridge) { }
+
+    prepare (
+        options: SessionOptions,
+        runtimeEnvironment: Record<string, string> = {},
+    ): Promise<PreparedSpawnRequest> {
+        return this.bridge.invoke('shell.prepareSpawn', {
+            command: options.command,
+            args: options.args,
+            cwd: options.cwd,
+            profileEnvironment: options.env,
+            runtimeEnvironment,
+            shellType: options.shellType,
+            loginShell: options.args.includes('-l') || options.args.includes('--login'),
+        })
+    }
+}
+
+function iconFor (icon?: string): string|undefined {
+    switch (icon) {
+        case 'terminal': return 'fas fa-terminal'
+        case 'alpine': return require('../../../tabby-electron/src/icons/alpine.svg')
+        case 'alma': return require('../../../tabby-electron/src/icons/alma.svg')
+        case 'clink': return require('../../../tabby-electron/src/icons/clink.svg')
+        case 'cmd': return require('../../../tabby-electron/src/icons/cmd.svg')
+        case 'cmder': return require('../../../tabby-electron/src/icons/cmder.svg')
+        case 'cmder-powershell': return require('../../../tabby-electron/src/icons/cmder-powershell.svg')
+        case 'cygwin': return require('../../../tabby-electron/src/icons/cygwin.svg')
+        case 'debian': return require('../../../tabby-electron/src/icons/debian.svg')
+        case 'docker': return require('../../../tabby-electron/src/icons/docker.svg')
+        case 'git-bash': return require('../../../tabby-electron/src/icons/git-bash.svg')
+        case 'kali': return require('../../../tabby-electron/src/icons/kali.svg')
+        case 'linux': return require('../../../tabby-electron/src/icons/linux.svg')
+        case 'msys2': return require('../../../tabby-electron/src/icons/msys2.svg')
+        case 'open-euler': return require('../../../tabby-electron/src/icons/open-euler.svg')
+        case 'oracle-linux': return require('../../../tabby-electron/src/icons/oracle-linux.svg')
+        case 'powershell': return require('../../../tabby-electron/src/icons/powershell.svg')
+        case 'powershell-core': return require('../../../tabby-electron/src/icons/powershell-core.svg')
+        case 'suse': return require('../../../tabby-electron/src/icons/suse.svg')
+        case 'ubuntu': return require('../../../tabby-electron/src/icons/ubuntu.svg')
+        case 'vs2017': return require('../../../tabby-electron/src/icons/vs2017.svg')
+        case 'vs2019': return require('../../../tabby-electron/src/icons/vs2019.svg')
+        case 'vs2022': return require('../../../tabby-electron/src/icons/vs2022.svg')
+        default: return undefined
+    }
+}
